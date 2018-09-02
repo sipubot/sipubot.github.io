184cb0c890bad580dc7173160875ee8fb380b97b
SIPU <ddasik00@naver.com>
2018-09-02 18:39:58 +0900

    corstest

---
+++ javascripts/testcors.js
@@ -0,0 +1,25 @@
+var invocation = new XMLHttpRequest();
+var url = 'https://sipu.iptime.org/Sipu/TestCors';
+   
+function callOtherDomain() {
+  if(invocation) {    
+    invocation.open('GET', url, true);
+    invocation.onreadystatechange = handler;
+    invocation.send(); 
+  }
+}
+
+function handler() {
+    if (invocation.readyState === XMLHttpRequest.DONE) {
+        if (invocation.status === 200) {
+          console.log(invocation.responseText);
+        } else {
+          console.log('request에 뭔가 문제가 있어요.');
+        }
+      }
+}
+
+document.getElementById('co').onclick(function (){
+    callOtherDomain();
+
+});
