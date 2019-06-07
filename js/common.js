4ba6804e150a50694f83fa441fdf4d3000297026
sipubot <ddasik00@naver.com>
2019-06-07 21:48:39 +0900

    ..

--- js/common.js
+++ js/common.js
@@ -205,10 +205,10 @@ var SIPUCOMMON = (function (SIPUCOMMON, $, undefined) {
 
     workers.qutget = function () {
         var n = new FETCHER();
-        n.triggerNode = NODES.BUTTON.Quot;
-        n.getNode = NODES.INPUT.Quot;
+        n.triggerNode = NODES.BUTTON.Quot[0];
+        n.getNode = NODES.INPUT.Quot[0];
         //n.nodeDataGet = 
-        n.setNode = NODES.UL.Quot;
+        n.setNode = NODES.UL.Quot[0];
         n.setPushType = "SET";
         n.setHTML = "";
         n.RqADD_URL = "/qut"
