ea91aefbac3e0c7ff9ec00775641429d7ae5510b
sipubot <ddasik00@naver.com>
2019-06-08 13:04:07 +0900

    ..

--- app/budget/js/common.js
+++ app/budget/js/common.js
@@ -268,16 +268,16 @@ var SIPUCOMMON = (function (SIPUCOMMON, $, undefined) {
     function setPageButton() {
         var tempAccount = `<tr><td><input type="text" class="form-control" placeholder="code" value="0001"></td><td><input type="text" class="form-control" placeholder="Name" value="신한"></td><td><button data-node="AccountDel" type="button" class="btn btn-secondary">삭제</button></td></tr>`;
         var tempCategory = `<tr><td><input type="checkbox" checked></td><select class="custom-select"><option value="false" selected>지출</option><option value="true">수입</option></select></td><td><input type="text" class="form-control" placeholder="code" value="0001"></td><td><input type="text" class="form-control" placeholder="Name" value="이체"></td><td><button data-node="CategoryDel" type="button" class="btn btn-secondary">삭제</button></td></tr>`;
-        $("BUTTON[data-node='CategoryNew']").click(() => {
+        $("BUTTON[data-node='CategoryNew']").click(function(){
             $("TBODY[data-node='CategoryTable']").append(tempCategory);
         });
-        $("BUTTON[data-node='AccountNew']").click(() => {
+        $("BUTTON[data-node='AccountNew']").click(function (){
             $("TBODY[data-node='AccountTable']").append(tempAccount);
         });
-        $("BUTTON[data-node='AccountDel']").click(() => {
+        $("BUTTON[data-node='AccountDel']").click(function (){
             $(this).parent().parent().remove();
         });
-        $("BUTTON[data-node='CategoryDel']").click(() => {
+        $("BUTTON[data-node='CategoryDel']").click(function (){
             $(this).parent().parent().remove();
         });
     }
