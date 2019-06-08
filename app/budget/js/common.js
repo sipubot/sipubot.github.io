e7a76a614061443dcfb56a6b5c2bf46fd7487419
sipubot <ddasik00@naver.com>
2019-06-08 17:25:19 +0900

    ..

--- app/budget/js/common.js
+++ app/budget/js/common.js
@@ -406,18 +406,19 @@ var SIPUCOMMON = (function (SIPUCOMMON, $, undefined) {
         n.setPushType = "ADD";
         n.setHTML = "";
         n.nodeDataSet = function (data) {
+            console.log(data);
             data.map(item =>{
                 if (!item.ttype) {
-                    self.setNode = $("INPUT[data-node='DataTableExpense']");
+                    self.setNode = $("TBODY[data-node='DataTableExpense']");
                 } else {
-                    self.setNode = $("INPUT[data-node='DataTableIncome']");
+                    self.setNode = $("TBODY[data-node='DataTableIncome']");
                 }
                 $(self.setNode).append(`<TR>
                 <td>${item.date}</td>
                 <td>${DATA.AccountHash[item.account_id]}</td>
-                <td>${DATA.CategoryHash[item.category_id]}</td>
+                <td>${DATA.CategoryHash[item.category_id][1]}</td>
                 <td>${item.amount}</td>
-                <th scope="row"><button value="${item.seq}" type="button" class="btn btn-secondary" onclick="javascript:SIPUCOMMON.delRow.setPage(this);">-</button></th>
+                <th scope="row"><button value="${item.seq}" type="button" class="btn btn-secondary" onclick="javascript:SIPUCOMMON.delRow.dataPage(this);">-</button></th>
                 </TR>`);
             });
         }
