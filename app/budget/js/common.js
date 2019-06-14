677c1c1493f480e5f75b25c0297f168fc8cd696d
SIPU <ddasik00@naver.com>
2019-06-14 12:09:36 +0900

    Update common.js

--- app/budget/js/common.js
+++ app/budget/js/common.js
@@ -591,9 +591,9 @@ var SIPUCOMMON = (function (SIPUCOMMON, $, undefined) {
         });
         re.push({
             date:'AVG',
-            income:data.reduce((s, a) => s + a.income, 0) / data.length,
-            expense:data.reduce((s, a) => s + a.expense, 0) / data.length,
-            differ:data.reduce((s, a) => s + a.differ, 0) / data.length
+            income:Math.round(data.reduce((s, a) => s + a.income, 0) / data.length),
+            expense:Math.round(data.reduce((s, a) => s + a.expense, 0) / data.length),
+            differ:Math.round(data.reduce((s, a) => s + a.differ, 0) / data.length)
         });
         return re;
     }
